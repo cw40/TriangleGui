6a3866d737d0d6f2e6d52e4111255133d63c0e71
cw40 <hcharliewilliams@gmail.com>
2020-09-24 13:43:20 -0500

    deleated cosine function

--- src/sample/TriangleMath.java
+++ src/sample/TriangleMath.java
@@ -21,17 +21,5 @@ public class TriangleMath {
             B = a;
         }
 
-        //finds the angle based off of the inverse of the sin
-        double bigAngle = Math.asin(A);
-        double smallAngle = Math.asin(B);
-
-        //adds to the array created at the start
-        finalAngles[0] = Math.toDegrees(bigAngle);
-        finalAngles[1] = Math.toDegrees(smallAngle);
-
-        //Returns the array made at the top
-        return finalAngles;
-    }
-
 
 }
